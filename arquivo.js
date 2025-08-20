212a66094a60acebcebdecb1982a2a36a8c670bb
Ana Luiza Alves de Lima <analuizalvesdelima@gmail.com>
2025-08-20 10:32:24 -0300

    Personagem agora é um objeto
    Fiz com que "Personagem" virasse um objeto

--- arquivo.js
+++ arquivo.js
@@ -1,37 +1,60 @@
-const personagem = document.getElementById("personagem");
-let posX = 0;
-let posY = 0;
-const passo = 5;
+class Personagem {
 
-document.addEventListener("keydown", (e) => { // função com parâmetro e, que vai ser substituido por algo dps
-    switch(e.key) { // escolha caso a tecla pressionada seja...
-        case "ArrowDown": // seta p/baixo
-        if(posY < 370) {
-        posY += passo;
-            personagem.style.top = posY + 'px';
-            personagem.style.backgroundPosition = '0px 0px';
-        } 
+    // atributos
+    elemento; // id do personagem no html
+    passo = 5;
+    posX = 0;
+    posY = 0;
+
+    // construtor
+    constructor(nome){
+        this.elemento = document.getElementById(nome);
+    }
+
+    constructor(){
+        this.elemento = document.getElementById("personagem");
+    }
+
+    // métodos
+    atualizarTela() {
+        this.elemento.style.left = this.posX + "px"; // atualiza a posição pelo css (por isso o style e o "px")
+        this.elemento.style.top = this.posY + "px"; // atualiza a posição pelo css (por isso o style e o "px")
+    }
+
+    mover(direcao) {
+        switch (direcao) {
+        case "baixo": // seta p/baixo
+            this.posY += passo;
             break;
-        case "ArrowUp": // seta p/cima
-        if(posY > 0) {
-            posY -= passo;
-            personagem.style.top = posY + 'px';
-            personagem.style.backgroundPosition = '0px 0px'; 
-        }
+        case "cima": // seta p/cima
+            posY += passo;        
             break;
-        case "ArrowLeft": // seta p/esq
-        if(posX > 0) {
+        case "esquerda": // seta p/esq
             posX -= passo;
-            personagem.style.left = posX + 'px';
-            personagem.style.backgroundPosition = '0px 0px'; 
-        }
             break;
-        case "ArrowRight": // seta p/dir
-        if(posX < 570) {
-            posX += passo;
-            personagem.style.left = posX + 'px';
-            personagem.style.backgroundPosition = '0px 0px';
-        }
+        case "direita": // seta p/dir
+            posX += passo;     
             break;
     }
-});
+        this.atualizarTela();
+        }  
+    }
+
+    // Instanciando um objeto do tipo personagem
+    const personagem = new Personagem("personagem");
+    document.addEventListener("keydown", (e) => {
+        switch(e.key) {
+            case "ArrowDown":
+                personagem.mover("baixo");
+                break;
+            case "ArrowUp":
+                personagem.mover("cima");
+                break;
+            case "ArrowLeft":
+                personagem.mover("esquerda");
+                break;
+            case "ArrowRight":
+                personagem.mover("direita");
+                break;
+        }
+    });
