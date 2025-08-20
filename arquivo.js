03a26c225e71cb2f1c44fcaea4d49e647e8def28
Ana Luiza Alves de Lima <analuizalvesdelima@gmail.com>
2025-08-20 11:17:11 -0300

    Update arquivo.js

--- arquivo.js
+++ arquivo.js
@@ -11,29 +11,32 @@ class Personagem {
         this.elemento = document.getElementById(nome);
     }
 
-    constructor(){
-        this.elemento = document.getElementById("personagem");
-    }
-
     // métodos
     atualizarTela() {
         this.elemento.style.left = this.posX + "px"; // atualiza a posição pelo css (por isso o style e o "px")
         this.elemento.style.top = this.posY + "px"; // atualiza a posição pelo css (por isso o style e o "px")
     }
 
+    // Criar um método de posição aleatória
+    aleatorio() {
+        this.posX = Math.floor(Math.random()) * 570;
+        this.posY = Math.floor(Math.random()) * 370;
+        this.atualizarTela;
+    }
+
     mover(direcao) {
         switch (direcao) {
         case "baixo": // seta p/baixo
-            this.posY += passo;
+            this.posY += this.passo;
             break;
         case "cima": // seta p/cima
-            posY += passo;        
+            this.posY -= this.passo;        
             break;
         case "esquerda": // seta p/esq
-            posX -= passo;
+            this.posX -= this.passo;
             break;
         case "direita": // seta p/dir
-            posX += passo;     
+            this.posX += this.passo;     
             break;
     }
         this.atualizarTela();
@@ -58,3 +61,23 @@ class Personagem {
                 break;
         }
     });
+
+    const personagem2 = new Personagem("inimigo");
+    document.addEventListener("keydown", (e) => {
+        switch(e.key) {
+            case "s":
+                personagem2.mover("baixo");
+                break;
+            case "w":
+                personagem2.mover("cima");
+                break;
+            case "a":
+                personagem2.mover("esquerda");
+                break;
+            case "d":
+                personagem2.mover("direita");
+                break;
+        }
+    });
+
+    personagem2.aleatorio();
